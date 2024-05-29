466d59ba85aedcda9750c6ba1462da6bed69769a
Jan Zaranski <jas.zaranski@gmail.com>
2024-05-29 08:07:05 +0200

    Init commit

---
+++ app/src/Repository/TaskRepository.php
@@ -0,0 +1,101 @@
+<?php
+/**
+ * Task repository.
+ */
+
+namespace App\Repository;
+
+use App\Entity\Category;
+use App\Entity\Task;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\QueryBuilder;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * Class TaskRepository.
+ *
+ * @method Task|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Task|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Task[]    findAll()
+ * @method Task[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ *
+ * @extends ServiceEntityRepository<Task>
+ *
+ * @psalm-suppress LessSpecificImplementedReturnType
+ */
+class TaskRepository extends ServiceEntityRepository
+{
+    /**
+     * Items per page.
+     *
+     * Use constants to define configuration options that rarely change instead
+     * of specifying them in configuration files.
+     * See https://symfony.com/doc/current/best_practices.html#configuration
+     *
+     * @constant int
+     */
+    public const PAGINATOR_ITEMS_PER_PAGE = 10;
+
+    /**
+     * Constructor.
+     *
+     * @param ManagerRegistry $registry Manager registry
+     */
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Task::class);
+    }
+
+    /**
+     * Query all records.
+     *
+     * @return QueryBuilder Query builder
+     */
+    public function queryAll(): QueryBuilder
+    {
+        return $this->getOrCreateQueryBuilder()
+            ->select(
+                'partial task.{id, createdAt, updatedAt, title}',
+                'partial category.{id, title}'
+            )
+            ->join('task.category', 'category')
+            ->orderBy('task.updatedAt', 'DESC');
+    }
+
+    /**
+     * Get or create new query builder.
+     *
+     * @param QueryBuilder|null $queryBuilder Query builder
+     *
+     * @return QueryBuilder Query builder
+     */
+    private function getOrCreateQueryBuilder(?QueryBuilder $queryBuilder = null): QueryBuilder
+    {
+        return $queryBuilder ?? $this->createQueryBuilder('task');
+    }
+
+    public function countByCategory(Category $category): int
+    {
+        return $this->createQueryBuilder('t')
+            ->select('count(t.id)')
+            ->where('t.category = :category')
+            ->setParameter('category', $category)
+            ->getQuery()
+            ->getSingleScalarResult();
+    }
+
+
+    public function findByCategory(Category $category): array
+    {
+        $entityManager = $this->getEntityManager();
+        $query = $entityManager
+            ->getRepository(Task::class)
+            ->createQueryBuilder('t')
+            ->where('t.category = :category')
+            ->setParameter('category', $category)
+            ->getQuery()
+            ->getResult();
+
+        return $query;
+    }
+}
---
+++ app/src/Service/CategoryService.php
@@ -0,0 +1,124 @@
+<?php
+/**
+ * Category service.
+ */
+
+namespace App\Service;
+
+use App\Entity\Category;
+use App\Repository\CategoryRepository;
+use App\Repository\TaskRepository;
+use Doctrine\ORM\Exception\ORMException;
+use Doctrine\ORM\NonUniqueResultException;
+use Doctrine\ORM\NoResultException;
+use Knp\Component\Pager\Pagination\PaginationInterface;
+use Knp\Component\Pager\PaginatorInterface;
+
+/**
+ * Class CategoryService.
+ */
+class CategoryService implements CategoryServiceInterface
+{
+    /**
+     * Paginator.
+     */
+    private PaginatorInterface $paginator;
+
+    /**
+     * Category repository.
+     */
+    private CategoryRepository $categoryRepository;
+
+    /**
+     * Transaction repository.
+     */
+    private TaskRepository $taskRepository;
+    private $TaskService;
+
+    /**
+     * Constructor.
+     *
+     * @param CategoryRepository $categoryRepository Category repository
+     * @param PaginatorInterface $paginator          Paginator
+     * @param TaskRepository     $taskRepository     Transaction repository
+     */
+    public function __construct(CategoryRepository $categoryRepository, PaginatorInterface $paginator, TaskRepository $taskRepository)
+    {
+        $this->categoryRepository = $categoryRepository;
+        $this->paginator = $paginator;
+        $this->taskRepository = $taskRepository;
+    }
+
+    /**
+     * Get paginated list.
+     *
+     * @param int $page Page number
+     *
+     * @return PaginationInterface<string, mixed> Paginated list
+     */
+    public function getPaginatedList(int $page): PaginationInterface
+    {
+        return $this->paginator->paginate(
+            $this->categoryRepository->queryAll(),
+            $page,
+            TaskRepository::PAGINATOR_ITEMS_PER_PAGE
+        );
+    }
+
+    /**
+     * Save entity.
+     *
+     * @param Category $category Category entity
+     * @throws ORMException
+     */
+    public function save(Category $category): void
+    {
+        if (null == $category->getId()) {
+            $category->setCreatedAt(new \DateTimeImmutable());
+        }
+        $category->setUpdatedAt(new \DateTimeImmutable());
+
+        $this->categoryRepository->save($category);
+    }
+
+    /**
+     * Delete entity.
+     *
+     * @param Category $category Category
+     * @throws ORMException
+     */
+    public function delete(Category $category): void
+    {
+        if (!$this->canBeDeleted($category)) {
+            throw new \Exception('Category cannot be deleted because it has associated tasks.');
+        }
+
+        $this->categoryRepository->delete($category);
+    }
+
+    public function canBeDeleted(Category $category): bool
+    {
+        try {
+            $result = $this->taskRepository->countByCategory($category);
+
+            return !($result > 0);
+        } catch (NoResultException|NonUniqueResultException) {
+            return false;
+        }
+    }
+
+
+    /**
+     * Find by id.
+     *
+     * @param int $id Category id
+     *
+     * @return Category|null Category entity
+     *
+     * @throws NonUniqueResultException
+     */
+    public function findOneById(int $id): ?Category
+    {
+        return $this->categoryRepository->findOneById($id);
+    }
+}
